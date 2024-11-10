d116c92d6acc0db517773e13d3b749bb05e985b2
Joonyoung <aimzero9303@gmail.com>
2024-11-10 22:48:53 +0900

    feat(home): 구글 검색 홈화면 추가 [코드 분리는 아직 x]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,90 @@
-import Image from "next/image";
+'use client'
+
+import { useEffect, useRef, useState } from "react"
+import { IoSearchOutline, IoCloseOutline } from "react-icons/io5"
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [searchValue, setSearchValue] = useState<string>('')
+  const [searchHistory, setSearchHistory] = useState<string[]>([])
+  const [isFocusSearchBar, setIsFocusSearchBar] = useState<boolean>(false)
+  const searchAreaRef = useRef<HTMLDivElement>(null)
+  
+  const submitSearch = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      const newSearchHistory = [...searchHistory, searchValue]
+      setSearchHistory(newSearchHistory)
+      localStorage.setItem('search-history', JSON.stringify(newSearchHistory))
+      window.open(`https://www.google.co.kr/search?q=${encodeURIComponent(searchValue)}`)
+    }
+  }
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const clickSearchHistory = (history: string) => {
+    window.open(`https://www.google.co.kr/search?q=${encodeURIComponent(history)}`)
+  }
+
+  const deleteHistory = (index: number) => {
+    const newSearchHistory = [...searchHistory]
+    newSearchHistory.splice(index, 1)
+    setSearchHistory(newSearchHistory)
+    localStorage.setItem('search-history', JSON.stringify(newSearchHistory))
+  }
+
+  const outSideClick = (event: MouseEvent | TouchEvent) => {
+    if (!searchAreaRef.current?.contains(event.target as Node)) {
+      setIsFocusSearchBar(false)
+    }
+  }
+
+  useEffect(() => {
+    const localStorageSearchHistory = localStorage.getItem('search-history')
+    if (localStorageSearchHistory) {
+      setSearchHistory(JSON.parse(localStorageSearchHistory))
+    }
+  }, [])
+
+  useEffect(() => {
+    window.addEventListener('mousedown', outSideClick)
+  }, [])
+
+  return (
+    <main className="flex flex-col items-center">
+      <div className="flex flex-col items-center mt-[15vh] gap-10 w-2/4 ">
+        {/* TODO 유저가 본인의 이름을 추가하면, 'Hello, OOO'이런 식으로 나와야함 */}
+        <h1 className="text-8xl">{`Hello`}</h1>
+        <div className="relative w-full flex justify-center" ref={searchAreaRef}>
+          <IoSearchOutline className="absolute top-1/3 left-5 text-lg text-slate-700 z-20" />
+          <input
+            className={`w-full h-12 rounded-3xl px-12 bg-slate-100 text-slate-700 ${!isFocusSearchBar || (isFocusSearchBar && searchHistory.length === 0) ? 'shadow-md' : ''} focus:outline-none z-10`}
+            value={searchValue}
+            onChange={(event) => setSearchValue(event.target.value)}
+            onKeyDown={(event) => submitSearch(event)}
+            onFocus={() => setIsFocusSearchBar(true)}
+            placeholder="Search Google"
+          />
+          {searchValue && (
+            <IoCloseOutline
+              className="absolute top-1/3 right-5 text-lg text-slate-700 cursor-pointer z-20"
+              onClick={() => setSearchValue('')}
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          )}
+          {searchHistory.length > 0 && isFocusSearchBar === true ? (
+            <ul className="absolute top-0 left-0 w-full pt-12 pb-2 bg-slate-100 text-slate-700
+              shadow-md rounded-3xl overflow-hidden">
+              {searchHistory.map((history, index) => (
+                <div key={index} className="relative px-12 hover:bg-slate-200">
+                  <li className="flex items-center h-12"
+                    onClick={() => clickSearchHistory(history)}>{history}
+                  </li>
+                  <IoCloseOutline
+                    className="absolute top-1/3 right-5 text-lg text-slate-700 cursor-pointer z-20"
+                    onClick={() => deleteHistory(index)}
+                  />
+                </div>
+              ))}
+            </ul>
+          ) : null}
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </div>
+    </main>
   );
 }
