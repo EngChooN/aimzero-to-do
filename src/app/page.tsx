f59a1b366b13f446fe66939c515533f2b9271e03
Joonyoung <aimzero9303@gmail.com>
2024-11-11 21:27:34 +0900

    fix(search): 한국어입력(IME) 입력 문제 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,14 @@ import { IoSearchOutline, IoCloseOutline } from "react-icons/io5"
 
 export default function Home() {
   const [searchValue, setSearchValue] = useState<string>('')
+  const [isComposition, setIsComposition] = useState<boolean>(false)
   const [searchHistory, setSearchHistory] = useState<string[]>([])
   const [isFocusSearchBar, setIsFocusSearchBar] = useState<boolean>(false)
   const searchAreaRef = useRef<HTMLDivElement>(null)
   
   const submitSearch = (event: React.KeyboardEvent) => {
-    if (event.key === 'Enter') {
-      const newSearchHistory = [...searchHistory, searchValue]
+    if (event.key === 'Enter' && !isComposition) {
+      const newSearchHistory = [searchValue, ...searchHistory]
       setSearchHistory(newSearchHistory)
       localStorage.setItem('search-history', JSON.stringify(newSearchHistory))
       window.open(`https://www.google.co.kr/search?q=${encodeURIComponent(searchValue)}`)
@@ -48,15 +49,17 @@ export default function Home() {
 
   return (
     <main className="flex flex-col items-center">
-      <div className="flex flex-col items-center mt-[15vh] gap-10 w-2/4 ">
+      <div className="flex flex-col items-center mt-[15vh] gap-10 max-w-2xl w-full ">
         {/* TODO 유저가 본인의 이름을 추가하면, 'Hello, OOO'이런 식으로 나와야함 */}
-        <h1 className="text-8xl">{`Hello`}</h1>
+        <h1 className="text-7xl">{`Hello`}</h1>
         <div className="relative w-full flex justify-center" ref={searchAreaRef}>
           <IoSearchOutline className="absolute top-1/3 left-5 text-lg text-slate-700 z-20" />
           <input
             className={`w-full h-12 rounded-3xl px-12 bg-slate-100 text-slate-700 ${!isFocusSearchBar || (isFocusSearchBar && searchHistory.length === 0) ? 'shadow-md' : ''} focus:outline-none z-10`}
             value={searchValue}
             onChange={(event) => setSearchValue(event.target.value)}
+            onCompositionStart={() => setIsComposition(false)}
+            onCompositionEnd={() => setIsComposition(true)}
             onKeyDown={(event) => submitSearch(event)}
             onFocus={() => setIsFocusSearchBar(true)}
             placeholder="Search Google"
