e94ba99b9f9a6a5c0712286db0fc453f5ddf7f4a
Joonyoung <aimzero9303@gmail.com>
2024-11-13 00:14:25 +0900

    fix: padding left 값 추가

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -3,7 +3,7 @@ import WeatherInfo from "./WeatherInfo/WeatherInfo"
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 flex items-center justify-between w-full h-14 pr-3 z-30">
+    <header className="fixed top-0 left-0 flex items-center justify-between w-full h-14 px-3 z-30">
       <WeatherInfo />
       <Setting />
     </header>
