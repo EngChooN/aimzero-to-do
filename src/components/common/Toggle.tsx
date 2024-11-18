c563ee5a96d94aa19ee59ee62239f8d97b5b55f3
Joonyoung <aimzero9303@gmail.com>
2024-11-18 15:57:41 +0900

    feat: 토글 컴포넌트 비활성 옵션 추가

--- src/components/common/Toggle.tsx
+++ src/components/common/Toggle.tsx
@@ -1,11 +1,19 @@
 interface Props {
   value: boolean
+  disabled?: boolean
   onClick: () => void
 }
 
-export default function Toggle({value, onClick}: Props) {
+export default function Toggle({value, disabled, onClick}: Props) {
   return (
-    <div onClick={onClick} className={`relative w-16 h-8 rounded-2xl p-[2px] flex ${value ? 'justify-end bg-green-600' : 'justify-start bg-slate-400'} items-center cursor-pointer`}>
+    <div onClick={!disabled ? onClick : undefined}
+      className={`
+        w-14 h-8 rounded-2xl p-[2px] flex 
+        ${value ? 'justify-end bg-green-500' : 'justify-start bg-slate-400'} 
+        ${disabled ? 'opacity-40 cursor-not-allowed' : 'cursor-pointer hover:opacity-90 active:scale-95'} 
+        items-center
+      `}
+    >
       <div className="size-7 bg-white rounded-full"></div>
     </div>
   )
