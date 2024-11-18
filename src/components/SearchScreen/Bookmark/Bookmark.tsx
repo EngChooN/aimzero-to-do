2e9a1425c8d4a37b8d40e4dbd1425b7b45aefe62
Joonyoung <aimzero9303@gmail.com>
2024-11-18 22:48:51 +0900

    feat: 디자인 개선

--- src/components/SearchScreen/Bookmark/Bookmark.tsx
+++ src/components/SearchScreen/Bookmark/Bookmark.tsx
@@ -1,5 +1,6 @@
 import { IBookmark, useBookmarkStore } from "@/store/useBookmarkStore"
 // import { IoEarth } from "react-icons/io5"
+import { IoAdd } from "react-icons/io5"
 
 export default function Bookmark() {
   const { bookmark, addBookmark } = useBookmarkStore()
@@ -14,12 +15,16 @@ export default function Bookmark() {
   }
 
   return(
-    <section className="flex gap-6 items-center justify-center">
-      <div onClick={handleAddBookmark}>+</div>
+    <section className="grid grid-cols-4 gap-6">
+      {bookmark.length < 8 && (
+        <div className="size-12 flex justify-center items-center cursor-pointer rounded-xl hover:bg-slate-50/20">
+          <IoAdd onClick={handleAddBookmark} className="text-2xl" />
+        </div>  
+      )}
       {bookmark.map((el, index) => (
-        <div className="flex flex-col items-center" key={index} onClick={() => window.open(el.url)}>
-          <img src={el.favicon} />
-          <p>{el.name}</p>
+        <div className="flex flex-col items-center justify-center bg-slate-50/20 hover:bg-slate-300/20 size-12 rounded-xl cursor-pointer" key={index} onClick={() => window.open(el.url)}>
+          <img src={el.favicon} className="size-6" />
+          {/* <p className="mt-[3px] text-[10px]">{el.name}</p> */}
         </div>
       ))}
     </section>
