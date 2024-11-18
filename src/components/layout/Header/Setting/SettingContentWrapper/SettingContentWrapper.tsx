934f76a99c0349125e0d86b677cb475209321ab6
Joonyoung <aimzero9303@gmail.com>
2024-11-18 13:40:47 +0900

    feat(setting): content wrapper 컴포넌트 label을 props로 받을 수 있게 개선

--- src/components/layout/Header/Setting/SettingContentWrapper/SettingContentWrapper.tsx
+++ src/components/layout/Header/Setting/SettingContentWrapper/SettingContentWrapper.tsx
@@ -1,7 +1,12 @@
-export default function SettingContentWrapper({children}: {children: React.ReactNode}) {
+interface IProps {
+  children: React.ReactNode
+  label: string
+}
+
+export default function SettingContentWrapper({ children, label }: IProps) {
   return (
     <div className="bg-slate-100/20 rounded-2xl py-3 px-2 text-gray-600">
-      <h2 className="mb-3">Theme</h2>
+      <h2 className="mb-3">{label}</h2>
       <div className="w-full overflow-auto flex">
         {children}
       </div>
