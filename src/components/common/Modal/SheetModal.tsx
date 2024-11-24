b4f9f2c15b0b740d9e8ed9291edac04aaf819afb
Joonyoung <aimzero9303@gmail.com>
2024-11-24 18:40:17 +0900

    feat: sheet modal 올라오는 높이 설정 및 애니메이션 동작 초 수정

--- src/components/common/Modal/SheetModal.tsx
+++ src/components/common/Modal/SheetModal.tsx
@@ -19,7 +19,7 @@ export default function SheetModal({ label, buttonLabel, buttonAction, child }: 
 
   return (
     <div className="p-3 flex-col gap-5 fixed top-0 left-0 size-full backdrop-blur-xl bg-slate-700/50 z-40 flex justify-center items-center animate-[fade-in_0.2s]">
-      <section ref={modalRef} className="flex flex-col max-w-[500px] w-full rounded-t-xl fixed bottom-0 left-auto overflow-hidden bg-gray-200/20 pb-5 animate-[pop-bottom_0.3s]">
+      <section ref={modalRef} className="flex flex-col max-w-[500px] w-full h-5/6 rounded-t-xl fixed bottom-0 left-auto overflow-hidden bg-gray-200/20 pb-5 animate-[pop-bottom_0.4s]">
         <div className="flex flex-col px-3 py-4">
           <div className="flex items-center justify-between">
             <button className="text-blue-400" onClick={() => closeModal()}>Cancel</button>
