e07bb4c8d713195e91bc9bab7ebef587423eb39f
Joonyoung <aimzero9303@gmail.com>
2025-01-06 17:48:06 +0900

    fix: search input이 bookmark보다 z-index가 낮아 생기는 화면 문제 수정

--- src/components/SearchScreen/SearchInput/SearchInput.tsx
+++ src/components/SearchScreen/SearchInput/SearchInput.tsx
@@ -63,7 +63,7 @@ export default function SearchInput() {
       )}
       {searchHistory.length > 0 && isFocusSearchBar === true ? (
         <ul className="transition-all duration-500 ease-in-out absolute top-0 left-0 w-full pt-14 pb-2 backdrop-blur-xl bg-slate-200/70 text-slate-700
-          shadow-md rounded-[28px] overflow-hidden animate-[fade-in_0.2s]">
+          shadow-md rounded-[28px] overflow-hidden animate-[fade-in_0.2s] z-10">
           {searchHistory.slice(0, 5).map((history, index) => (
             <div key={index} className="relative px-14 hover:bg-slate-200/30">
               <IoTimerOutline className="absolute top-1/3 left-6 text-lg text-slate-700 z-20" />
